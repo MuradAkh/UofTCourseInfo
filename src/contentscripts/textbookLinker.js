f9e17bfd4e782078d0bb54978c4e0e0ce8c34d57
Murad Akhundov <muradakhdev@gmail.com>
2019-01-29 16:29:16 -0500

    combined installed/uninstalled htmls

--- src/contentscripts/textbookLinker.js
+++ src/contentscripts/textbookLinker.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
     $('#settings-link').attr('href', getSettingsUrl());
     $('#about-link').attr('href', getAboutUrl());
+    $('#installed').forEach(() => this.removeAttr('hidden'));
+    $('#not-installed').forEach(() => this.attr('hidden'))
 });
